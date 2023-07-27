6e58332b0c4776531c5a143743633d9c66e0ad33
Bruno Barros <brunomiguelbarrospinto@gmail.com>
2023-07-27 13:40:20 +0200

    Test css and scss imports

--- app.vue
+++ app.vue
@@ -9,9 +9,15 @@
 </template>
 
 <script setup>
+import "~/assets/css/styles.css";
+
 const runtimeConfig = useRuntimeConfig();
 const appConfig = useAppConfig();
 
 console.log(runtimeConfig);
 console.log(appConfig);
 </script>
+
+<style lang="scss">
+@use "~/assets/scss/styles.scss";
+</style>
